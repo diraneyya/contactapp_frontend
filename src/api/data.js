eb02879be11a0840dbbfd34e03871aa5ce9a3f06
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2023-03-18 01:30:10 +0100

    add data

---
+++ src/api/data.js
@@ -0,0 +1,41 @@
+const contacts = [
+  {
+    id: "5ev7xcu",
+    createdAt: 1678806647759,
+    first: "Jack",
+    last: "hohoh",
+    twitter: "@jackhohoho",
+    avatar: "https://thumbs.dreamstime.com/z/funny-face-baby-27701492.jpg",
+    notes: "milk ?",
+  },
+  {
+    id: "t52em94",
+    createdAt: 1678806678721,
+    first: "Mohammed",
+    last: "Biryani",
+    twitter: "@glassesMohammed",
+    avatar: "https://cdn.britannica.com/64/121664-004-73C27A0C/Steve-Jobs.jpg?s=1500x700&q=85",
+    notes: "He has glasses 🤓",
+  },
+  {
+    id: "heq5of8",
+    createdAt: 1678802542011,
+    first: "Orwa",
+    last: "Diraneyya",
+    twitter: "@ODiraneyya",
+    avatar:
+      "https://yt3.googleusercontent.com/ytc/AL5GRJXnjrU7kq-g_FhDSbPzOfp_MDITcF3_SlHgUwiCBQ=s176-c-k-c0x00ffffff-no-rj",
+    notes: "A poor man 😝",
+  },
+  {
+    id: "3emys0s",
+    createdAt: 1678803228972,
+    first: "Steve",
+    last: "Jobs",
+    twitter: "@dead",
+    avatar: "https://cdn.britannica.com/04/171104-050-AEFE3141/Steve-Jobs-iPhone-2010.jpg",
+    notes: "Not with us anymore 🥲",
+  },
+];
+
+export default contacts; 
